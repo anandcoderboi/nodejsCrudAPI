bba511919d3562e5be1b69e556c827c7a08d0df6
anandcoderboi <anandbittce@gmail.com>
2022-12-04 20:18:28 +0530

    added files

--- routes/users.js
+++ routes/users.js
@@ -8,17 +8,31 @@ const router = express.Router();
 let users = [];
 
 router.get("/", (req, res) => {
-  res.send(users);
+  res.send({
+    success: true,
+    messege: "data fetched successfully",
+    data: users,
+  });
 });
 
 router.post("/", (req, res) => {
   const user = req.body;
-  const userId = uuidv4();
+  const firstName = user.firstName;
 
-  const userWithId = { ...user, id: userId };
-  users.push({ ...user, id: uuidv4() });
+  if (firstName) {
+    const userId = uuidv4();
+    console.log(user);
+    const userWithId = { ...user, id: userId };
+    users.push({ ...user, id: uuidv4() });
 
-  res.send(`user with the name ${user.firsttName} added to the database`);
+    res.send(`user with the name ${user.firstName} added to the database`);
+  } else {
+    res.send({
+      success: false,
+      messege: "validation error",
+      errors: [{ field: "firstName", messege: "cannot be empty" }],
+    });
+  }
 });
 
 router.get("/:id", (req, res) => {
@@ -32,20 +46,19 @@ router.delete("/:id", (req, res) => {
   const { id } = req.params;
 
   users = users.filter((user) => user.id !== id);
+
+  res.send(`user with the ${id} has been deleted`);
 });
 
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+  const user = users.find((user) => user.id === id);
 
-
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const{firstName,lastName,age}=req.body
-    const user= users.find((user)=>user.id === id);
-
-    if(firstName) user.firstName = firstName
-    if(lastName) user.lastName = lastName
-    if(age) user.age = age
-    res.send('user with the ${id} has been updated')
-
-})
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (age) user.age = age;
+  res.send(`user with the ${id} has been updated`);
+});
 
 export default router;
